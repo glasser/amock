17e1e951663c560a441f03396cd96f6f27b8f0da
David Glasser <glasser@davidglasser.net>
2006-11-29 23:38:52 +0000

    refactor file structure a little more

--- src/ruby/process_trace.rb
+++ src/ruby/process_trace.rb
@@ -4,6 +4,7 @@ require 'optparse'
 require 'ostruct'
 
 require 'junit-gen'
+require 'trace-handlers'
 
 def main
   opts = OptionParser.new
@@ -27,7 +28,7 @@ def main
     exit
   end
 
-  processor = JUnitGenerator.new(os.classname)
+  processor = TestClassGenerator.new(os.classname)
 
   os.doc.root.each_element do |e|
     processor.process_action e
@@ -47,46 +48,6 @@ end
 
 def internal_to_external_classname(classname)
   classname.gsub '/', '.'
-end
-  
-class TestProcessorHandler; end
-
-class WaitForConstructorToEnd < TestProcessorHandler
-  def process_action(action, sm)
-    if trace_id = action_is_constructor(action, sm.generator.classname) and
-        trace_id == sm.trace_id
-
-      sm << sm.build_constructor(sm.generator.classname, action.elements['args'])
-      
-      sm.next_state(MonitorCallsOnObject.new)
-    end
-  end
-end
-
-class MonitorCallsOnObject < TestProcessorHandler
-  def process_action(action, sm)
-    if action.name == 'preCall' and 
-        action.elements["receiver[object[@id=#{sm.trace_id}]]"]
-      # We'll print out this call when it returns.
-      sm.next_state(WaitForMethodToEnd.new(action.attributes['call']))
-    end
-  end
-end
-
-class WaitForMethodToEnd < TestProcessorHandler
-  def initialize(callid)
-    @callid = callid
-  end
-  
-  def process_action(action, sm)
-    if action.name == 'postCall' and action.attributes['call'] == @callid
-      retval = action.elements['void'] ? nil : action.elements['return'].elements[1]
-      sm << sm.build_method_call(action.attributes['name'],
-                                 action.elements['args'], retval)
-
-      sm.next_state(MonitorCallsOnObject.new)
-    end
-  end
 end
 
 main

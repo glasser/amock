2c88d0b0544fb4096cd8273212aed7b2b20008c3
David Glasser <glasser@davidglasser.net>
2006-12-26 01:44:29 +0000

    Start writing the test class representation, TDD-style.

---
+++ src/java/edu/mit/csail/pag/amock/representation/TestClassGenerator.java
@@ -0,0 +1,11 @@
+package edu.mit.csail.pag.amock.representation;
+
+import java.io.PrintStream;
+
+public class TestClassGenerator {
+    public TestClassGenerator(String testClassName) {
+    }
+
+    public void printSource(PrintStream ps) {
+    }
+}
---
+++ src/java/edu/mit/csail/pag/amock/tests/TestClassGeneratorTests.java
@@ -0,0 +1,31 @@
+package edu.mit.csail.pag.amock.tests;
+
+import org.jmock.MockObjectTestCase;
+import org.jmock.Mock;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import edu.mit.lcs.pag.textutil.ReportChecker;
+
+import edu.mit.csail.pag.amock.representation.TestClassGenerator;
+
+public class TestClassGeneratorTests extends MockObjectTestCase {
+    public void testEmptyTestClassGenerator() throws Exception {
+        TestClassGenerator tcg = new TestClassGenerator("MyGeneratedTests");
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        tcg.printSource(new PrintStream(baos));
+
+        ReportChecker r = ReportChecker.create(baos);
+
+        r.c("package edu.mit.csail.pag.subject.generated;");
+        r.c("");
+        r.c("import org.jmock.MockObjectTestCase;");
+        r.c("import org.jmock.Mock;");
+        r.c("");
+        r.c("public class MyGeneratedTests extends MockObjectTestCase {");
+        r.c("}");
+    }
+}

d232ddecbc713cc25a22670500d4e0135c9a6bd9
David Glasser <glasser@davidglasser.net>
2006-12-26 20:51:32 +0000

    Test that printSource is appropriately called on TestMethodGenerators

--- src/java/edu/mit/csail/pag/amock/tests/TestCaseGeneratorTests.java
+++ src/java/edu/mit/csail/pag/amock/tests/TestCaseGeneratorTests.java
@@ -3,14 +3,11 @@ package edu.mit.csail.pag.amock.tests;
 import org.jmock.MockObjectTestCase;
 import org.jmock.Mock;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 
 import java.util.Arrays;
 
-import edu.mit.lcs.pag.textutil.ReportChecker;
-
-import edu.mit.csail.pag.amock.representation.TestCaseGenerator;
+import edu.mit.csail.pag.amock.representation.*;
 
 public class TestCaseGeneratorTests extends MockObjectTestCase {
     private void expectLine(Mock app, String s) {
@@ -46,7 +43,7 @@ public class TestCaseGeneratorTests extends MockObjectTestCase {
         expectLine(a, "}");
     }
     
-    public void testEmptyTestCaseGenerator() throws Exception {
+    public void testEmptyTestCaseGenerator() throws IOException {
         TestCaseGenerator tcg = new TestCaseGenerator("MyGeneratedTests");
         Mock app = mock(Appendable.class);
 
@@ -59,4 +56,36 @@ public class TestCaseGeneratorTests extends MockObjectTestCase {
 
         tcg.printSource((Appendable) app.proxy());
     }
+
+    public void testMockedMethodGenerators() throws IOException {
+        TestCaseGenerator tcg = new TestCaseGenerator("MyGeneratedTests");
+        Mock app = mock(Appendable.class);
+        Mock mg1 = mock(TestMethodGenerator.class);
+        Mock mg2 = mock(TestMethodGenerator.class);
+        Appendable proxyApp = (Appendable) app.proxy();
+
+        expectPackage(app);
+        expectImports(app,
+                      "org.jmock.MockObjectTestCase",
+                      "org.jmock.Mock");
+        expectClassHeader(app, "MyGeneratedTests");
+        expectClassFooter(app);
+
+        tcg.addMethodGenerator((TestMethodGenerator) mg1.proxy());
+        tcg.addMethodGenerator((TestMethodGenerator) mg2.proxy());
+
+        mg1.expects(once())
+            .method("printSource").with(same(proxyApp)).isVoid()
+            .id("called PS");
+
+        expectLine(app, "");
+
+        mg2.expects(once())
+            .method("printSource").with(same(proxyApp))
+            .after(mg1, "called PS")
+            .isVoid();
+        
+        tcg.printSource(proxyApp);
+    }
+
 }

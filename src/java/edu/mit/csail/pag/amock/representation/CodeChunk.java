5c25da5f55c278a07e1a31f81fecd8b89da5ed2b
David Glasser <glasser@davidglasser.net>
2006-12-27 20:36:45 +0000

    Remove some spurious "throws IOException"s.

--- src/java/edu/mit/csail/pag/amock/representation/CodeChunk.java
+++ src/java/edu/mit/csail/pag/amock/representation/CodeChunk.java
@@ -1,10 +1,10 @@
 package edu.mit.csail.pag.amock.representation;
 
-import java.io.IOException;
+
 
 public interface CodeChunk {
     /**
      * Print the source of this chunk to the given LinePrinter.
      */
-    public void printSource(LinePrinter a) throws IOException;
+    public void printSource(LinePrinter a);
 }

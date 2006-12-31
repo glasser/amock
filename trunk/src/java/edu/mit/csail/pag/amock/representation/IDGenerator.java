d839c22b3a5cc97748a04dff16834e8c3fda1d64
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2006-12-31 23:41:55 +0000

    String tests together more linearly.
    Note that a test is failing now.

---
+++ trunk/src/java/edu/mit/csail/pag/amock/representation/IDGenerator.java
@@ -0,0 +1,6 @@
+package edu.mit.csail.pag.amock.representation;
+
+public interface IDGenerator {
+    public String getLastID();
+    public String getNextID();
+}
--- trunk/src/java/edu/mit/csail/pag/amock/tests/AmockUnitTestCase.java
+++ trunk/src/java/edu/mit/csail/pag/amock/tests/AmockUnitTestCase.java
@@ -1,11 +1,18 @@
 package edu.mit.csail.pag.amock.tests;
 
 import edu.mit.csail.pag.amock.jmock.MockObjectTestCase;
+import edu.mit.csail.pag.amock.representation.*;
 
 import org.jmock.Mock;
 import org.jmock.core.Constraint;
+import org.jmock.builder.*;
+
+import java.util.*;
 
 public abstract class AmockUnitTestCase extends MockObjectTestCase {
+    private Map<Mock, IDGenerator> idGeneratorsForMocks
+        = new HashMap<Mock, IDGenerator>();
+        
     /**
      * Expect a line on a Mocked LinePrinter.
      */
@@ -17,12 +24,39 @@ public abstract class AmockUnitTestCase extends MockObjectTestCase {
      * Expect some lines on a Mocked LinePrinter.
      */
     protected void expectLines(Mock lp, String... strs) {
+        String lastId = getLastID(lp);
+        String thisId = getThisID(lp);
+        
         Constraint[][] specs = new Constraint[strs.length][];
         for (int i = 0; i < strs.length; i++) {
             specs[i] = args(eq(strs[i]));
         }
-        lp.expects(with(specs))
-            .method("line");
+
+        MatchBuilder mb
+            = lp.expects(with(specs))
+                .method("line");
+
+        if (lastId != null) {
+            mb = mb.after(lastId);
+        }
+
+        mb.id(thisId);
+    }
+
+    private String getLastID(Mock lp) {
+        if (idGeneratorsForMocks.containsKey(lp)) {
+            return idGeneratorsForMocks.get(lp).getLastID();
+        } else {
+            return null;
+        }
+    }
+
+    private String getThisID(Mock lp) {
+        if (! idGeneratorsForMocks.containsKey(lp)) {
+            idGeneratorsForMocks.put(lp,
+                                     new PrefixCountingIDGenerator("line "));
+        }
+        return idGeneratorsForMocks.get(lp).getNextID();
     }
 }
     

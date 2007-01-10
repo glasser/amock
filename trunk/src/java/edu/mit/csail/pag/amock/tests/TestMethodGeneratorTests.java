717d8eefca3ebf7a493f3396957b6a2a3277321f
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-01-10 22:35:19 +0000

    Add to the test the creation of two more mocks, of the same type.

--- trunk/src/java/edu/mit/csail/pag/amock/tests/TestMethodGeneratorTests.java
+++ trunk/src/java/edu/mit/csail/pag/amock/tests/TestMethodGeneratorTests.java
@@ -20,17 +20,29 @@ public class TestMethodGeneratorTests extends AmockUnitTestCase {
                     "  // Create mocks.",
                     "  Mock mockCookieJar = mock(CookieJar.class);",
                     "  CookieJar proxyCookieJar = (CookieJar) mockCookieJar.proxy();",
+                    "  Mock mockCookie = mock(Cookie.class);",
+                    "  Cookie proxyCookie = (Cookie) mockCookie.proxy();",
+                    "  Mock mockCookie1 = mock(Cookie.class);",
+                    "  Cookie proxyCookie1 = (Cookie) mockCookie1.proxy();",
                     "  ",
                     "  // Set up expectations.",
                     "  ",
                     "  // Run the code under test.",
                     "}");
+        
         resolver.expects(once())
             .method("getSourceName")
             .with(eq("edu.mit.csail.pag.amock.subjects.bakery.CookieJar"))
             .will(returnValue("CookieJar"));
+        resolver.expects(exactly(2))
+            .method("getSourceName")
+            .with(eq("edu.mit.csail.pag.amock.subjects.bakery.Cookie"))
+            .will(returnValue("Cookie"));
+
 
         tmg.addMock("edu.mit.csail.pag.amock.subjects.bakery.CookieJar");
+        tmg.addMock("edu.mit.csail.pag.amock.subjects.bakery.Cookie");
+        tmg.addMock("edu.mit.csail.pag.amock.subjects.bakery.Cookie");
         
         tmg.printSource((LinePrinter) app.proxy());
     }

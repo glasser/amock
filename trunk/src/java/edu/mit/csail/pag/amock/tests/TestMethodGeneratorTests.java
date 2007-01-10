e2714140476d80e8382df002ec6b8c0625274720
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-01-10 22:28:15 +0000

    Make it possible for XOc

--- trunk/src/java/edu/mit/csail/pag/amock/tests/TestMethodGeneratorTests.java
+++ trunk/src/java/edu/mit/csail/pag/amock/tests/TestMethodGeneratorTests.java
@@ -9,18 +9,29 @@ import edu.mit.csail.pag.amock.representation.*;
 
 public class TestMethodGeneratorTests extends AmockUnitTestCase {
     public void testEmptyMethodGenerator() {
-        TestMethodGenerator tmg = new TestMethodGenerator("fooAndBar");
+        Mock resolver = mock(ClassNameResolver.class);
+        TestMethodGenerator tmg
+            = new TestMethodGenerator("fooAndBar",
+                                      (ClassNameResolver) resolver.proxy());
         Mock app = mock(LinePrinter.class);
 
         expectLines(app,
                     "public void testFooAndBar {",
                     "  // Create mocks.",
+                    "  Mock mockCookieJar = mock(CookieJar.class);",
+                    "  CookieJar proxyCookieJar = (CookieJar) mockCookieJar.proxy();",
                     "  ",
                     "  // Set up expectations.",
                     "  ",
                     "  // Run the code under test.",
                     "}");
+        resolver.expects(once())
+            .method("getSourceName")
+            .with(eq("edu.mit.csail.pag.amock.subjects.bakery.CookieJar"))
+            .will(returnValue("CookieJar"));
 
+        tmg.addMock("edu.mit.csail.pag.amock.subjects.bakery.CookieJar");
+        
         tmg.printSource((LinePrinter) app.proxy());
     }
 }

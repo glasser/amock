e71dcc55a75956263596131151df5dd766fd8ded
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-01-17 23:49:43 +0000

    Move one more test to jMock 2.

--- trunk/src/java/edu/mit/csail/pag/amock/tests/IndentingLinePrinterTests.java
+++ trunk/src/java/edu/mit/csail/pag/amock/tests/IndentingLinePrinterTests.java
@@ -1,27 +1,19 @@
 package edu.mit.csail.pag.amock.tests;
 
-import org.jmock.MockObjectTestCase;
-import org.jmock.Mock;
-
-import org.jmock.core.Constraint;
-
 import edu.mit.csail.pag.amock.representation.*;
-import edu.mit.csail.pag.amock.jmock.*;
 
-import java.util.*;
-
-public class IndentingLinePrinterTests extends JMock1AmockUnitTestCase {
+public class IndentingLinePrinterTests extends AmockUnitTestCase {
     public void testIndentingLinePrinter() {
-        Mock lp = mock(LinePrinter.class);
+        LinePrinter lp = mock(LinePrinter.class);
 
         IndentingLinePrinter outer
-            = new IndentingLinePrinter((LinePrinter) lp.proxy(), 2);
+            = new IndentingLinePrinter(lp, 2);
         IndentingLinePrinter inner = new IndentingLinePrinter(outer, 4);
 
-        expectLines(lp,
-                    "  printed from outer",
-                    "      printed from inner",
-                    "  outer again");
+        expects(lines(lp,
+                      "  printed from outer",
+                      "      printed from inner",
+                      "  outer again"));
 
         outer.line("printed from outer");
         inner.line("printed from inner");

04e4ce951c98cd1f65fcad3d0cc6fd06e7b20166
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-01-18 07:27:29 +0000

    Convert another test to jMock 2.

--- trunk/src/java/edu/mit/csail/pag/amock/tests/CodeChunkAndBlockTests.java
+++ trunk/src/java/edu/mit/csail/pag/amock/tests/CodeChunkAndBlockTests.java
@@ -1,7 +1,5 @@
 package edu.mit.csail.pag.amock.tests;
 
-import org.jmock.Mock;
-
 import edu.mit.csail.pag.amock.representation.*;
 
 public class CodeChunkAndBlockTests extends AmockUnitTestCase {

b656ec7c8104cf0dfe737e1ad8d0da6c362692aa
David Glasser <glasser@davidglasser.net>
2007-01-20 01:37:09 +0000

    Factor out XStream.

--- src/java/edu/mit/csail/pag/amock/trace/Serializer.java
+++ src/java/edu/mit/csail/pag/amock/trace/Serializer.java
@@ -1,24 +1,10 @@
 package edu.mit.csail.pag.amock.trace;
 
-import com.thoughtworks.xstream.XStream;
 import java.io.PrintStream;
 
 public class Serializer {
     private final XStream xs = new XStream();
-
-    {
-        xs.alias("pre-call", PreCall.class);
-        xs.alias("post-call", PostCall.class);
-        xs.alias("instance", Instance.class);
-        xs.alias("primitive", Primitive.class);
-        xs.alias("void-return-value", VoidReturnValue.class);
-        xs.alias("initializing-object", ConstructorReceiver.class);
-
-        xs.useAttributeFor("id", int.class);
-        xs.useAttributeFor("className", String.class);
-        xs.useAttributeFor("callId", int.class);
-    }
-
+    
     private final PrintStream ps;
 
     public Serializer(PrintStream ps) {

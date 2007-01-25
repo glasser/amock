518058646fe1b48138f225f4ee825dc26354f007
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-01-25 20:29:36 +0000

    Add equals and hashCode to the TraceObject implementations.

--- trunk/src/java/edu/mit/csail/pag/amock/trace/Instance.java
+++ trunk/src/java/edu/mit/csail/pag/amock/trace/Instance.java
@@ -1,11 +1,25 @@
 package edu.mit.csail.pag.amock.trace;
 
 public class Instance extends TraceObject {
-    private final String className;
-    private final int id;
+    public final String className;
+    public final int id;
 
     public Instance(String className, int id) {
         this.className = className;
         this.id = id;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (!(o instanceof Instance)) {
+            return false;
+        }
+        Instance i = (Instance) o;
+
+        return className.equals(i.className) &&
+            id == i.id;
+    }
+
+    @Override public int hashCode() {
+        return className.hashCode() + 37*id;
     }
 }

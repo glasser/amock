3a20e296e5b184c17578b8ef301ad25e87a3c817
David Glasser <glasser@davidglasser.net>
2007-01-25 20:29:36 +0000

    Add equals and hashCode to the TraceObject implementations.

--- src/java/edu/mit/csail/pag/amock/trace/Primitive.java
+++ src/java/edu/mit/csail/pag/amock/trace/Primitive.java
@@ -6,4 +6,17 @@ public class Primitive extends TraceObject {
     public Primitive(Object value) {
         this.value = value;
     }
+
+    @Override public boolean equals(Object o) {
+        if (!(o instanceof Primitive)) {
+            return false;
+        }
+        Primitive p = (Primitive) o;
+
+        return value.equals(p.value);
+    }
+
+    @Override public int hashCode() {
+        return value.hashCode() * 3;
+    }
 }

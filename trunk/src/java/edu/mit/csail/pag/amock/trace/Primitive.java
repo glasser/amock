9f08dc49fef0b553d6dbd6806aea9d73ae82b65e
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-01-29 23:51:30 +0000

    Implement ProgramObject support for Primitive.

--- trunk/src/java/edu/mit/csail/pag/amock/trace/Primitive.java
+++ trunk/src/java/edu/mit/csail/pag/amock/trace/Primitive.java
@@ -24,6 +24,45 @@ public class Primitive extends TraceObject implements ProgramObject {
 
     // Implements ProgramObject method.
     public String getSourceRepresentation() {
-        return null;
+        if (value == null) {
+            return "null";
+        } else if (value instanceof String) {
+            return quoteString((String) value);
+        } else if (value instanceof Character) {
+            return "'" + quoteChar((Character) value) + "'";
+        } else {
+            // Should be numeric.
+            return value.toString();
+        }
+    }
+        
+    private String quoteString(String unformatted) {
+        StringBuffer formatted = new StringBuffer();
+        formatted.append('"');
+        for (int i = 0; i < unformatted.length(); i++) {
+            formatted.append(quoteChar(unformatted.charAt(i)));
+        }
+        formatted.append('"');
+        
+        return formatted.toString();
+    }
+
+    private String quoteChar(char ch) {
+        switch (ch) {
+        case '"':
+            return "\\\"";
+        
+        case '\n':
+            return "\\n";
+            
+        case '\r':
+            return "\\r";
+        
+        case '\t':
+            return "\\t";
+        
+        default:
+            return new String(new char[]{ch});
+        }
     }
 }

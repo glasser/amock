2ea4a229f589e3e5aed4083f61388a7b426aec17
David Glasser <glasser@davidglasser.net>
2007-01-31 02:35:20 +0000

    Allow the constructors of the primary objects to take arguments.

--- src/java/edu/mit/csail/pag/amock/representation/Primary.java
+++ src/java/edu/mit/csail/pag/amock/representation/Primary.java
@@ -3,10 +3,13 @@ package edu.mit.csail.pag.amock.representation;
 public class Primary implements ProgramObject {
     private final String classSourceName;
     private final String varBaseName;
+    private final ProgramObject[] constructorArguments;
 
-    public Primary(String classSourceName, String varBaseName) {
+    public Primary(String classSourceName, String varBaseName,
+                   ProgramObject[] constructorArguments) {
         this.classSourceName = classSourceName;
         this.varBaseName = varBaseName;
+        this.constructorArguments = constructorArguments;
     }
 
     public String getClassSourceName() {
@@ -18,7 +21,26 @@ public class Primary implements ProgramObject {
     }
 
     public String getConstructor() {
-        return "new " + getClassSourceName() + "()";
+        StringBuilder s = new StringBuilder();
+        s.append("new ");
+        s.append(getClassSourceName());
+        s.append("(");
+
+        boolean first = true;
+
+        for (ProgramObject o : constructorArguments) {
+            if (first) {
+                first = false;
+            } else {
+                s.append(", ");
+            }
+
+            s.append(o.getSourceRepresentation());
+        }
+
+        s.append(")");
+        
+        return s.toString();
     }
 
     // Implements ProgramObject method.

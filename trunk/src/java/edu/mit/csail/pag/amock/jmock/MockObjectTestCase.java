5ac60c6dd0304c13f4d70cdbf1b06c839fd91767
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-03-28 19:32:59 +0000

    Upgrade from a random snapshot of jMock 2 development to the 2.0.0 RC1. Note that this requires including the UnsafeHackConcreteClassImposteriser myself because they won't distribute it in jar form anymore.
    All sorts of things now don't compile.

--- trunk/src/java/edu/mit/csail/pag/amock/jmock/MockObjectTestCase.java
+++ trunk/src/java/edu/mit/csail/pag/amock/jmock/MockObjectTestCase.java
@@ -1,7 +1,5 @@
 package edu.mit.csail.pag.amock.jmock;
 
-import org.jmock.lib.nonstd.UnsafeHackConcreteClassImposteriser;
-
 public abstract class MockObjectTestCase
     extends org.jmock.integration.junit3.MockObjectTestCase {
     {

a7743a7808d4bf7e9378393635eba6ec4d56c749
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-04-16 18:06:24 +0000

    Upgrade to a CVS snapshot of jmock2 (which may become 2.1.0) which uses objenesis to do class mocking.

--- trunk/src/java/edu/mit/csail/pag/amock/jmock/MockObjectTestCase.java
+++ trunk/src/java/edu/mit/csail/pag/amock/jmock/MockObjectTestCase.java
@@ -1,11 +1,12 @@
 package edu.mit.csail.pag.amock.jmock;
 
 import org.jmock.Sequence;
+import org.jmock.lib.objenesis.ObjenesisImposteriser;
 
 public abstract class MockObjectTestCase
     extends org.jmock.integration.junit3.MockObjectTestCase {
     {
-        setImposteriser(new UnsafeHackConcreteClassImposteriser());
+        setImposteriser(ObjenesisImposteriser.INSTANCE);
     }
 
     protected Sequence s = sequence("ordering");

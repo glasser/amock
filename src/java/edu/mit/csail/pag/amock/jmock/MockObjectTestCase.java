59946948b9bba4bb0185450d46f83efa9e62a118
David Glasser <glasser@davidglasser.net>
2007-04-23 20:05:50 +0000

    r117165@macbook:  glasser | 2007-04-22 16:00:14 -0400  Make our MockObjectTestCase warn if you try to mock  a class with a non-static public field.  (On the JModeller  trace, this is the case for Rectangle.)

--- src/java/edu/mit/csail/pag/amock/jmock/MockObjectTestCase.java
+++ src/java/edu/mit/csail/pag/amock/jmock/MockObjectTestCase.java
@@ -2,6 +2,8 @@ package edu.mit.csail.pag.amock.jmock;
 
 import org.jmock.Sequence;
 import org.jmock.lib.objenesis.ObjenesisImposteriser;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public abstract class MockObjectTestCase
     extends org.jmock.integration.junit3.MockObjectTestCase {
@@ -10,4 +12,26 @@ public abstract class MockObjectTestCase
     }
 
     protected Sequence s = sequence("ordering");
+
+    @Override public <T> T mock(Class<T> typeToMock, String name) {
+        checkMockability(typeToMock);
+        return super.mock(typeToMock, name);
+    }
+
+    @Override public <T> T mock(Class<T> typeToMock) {
+        checkMockability(typeToMock);
+        return super.mock(typeToMock);
+    }
+
+    private <T> void checkMockability(Class<T> typeToMock) {
+        for (Field f : typeToMock.getFields()) {
+            int mods = f.getModifiers();
+            if (Modifier.isPublic(mods) && !Modifier.isStatic(mods)) {
+                // XXX maybe only care about non-final fields?
+                System.err.println("WARNING: Trying to mock class " +
+                                   typeToMock + " with public field: "
+                                   + f);
+            }
+        }
+    }
 }

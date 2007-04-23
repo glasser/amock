6101fcb516ad1cea2fd47734bdff801d45e705c4
David Glasser <glasser@davidglasser.net>
2007-04-23 20:06:03 +0000

    r117166@macbook:  glasser | 2007-04-22 16:06:30 -0400  Check for mocking classes with final methods (but make both  types of warnings configurable with a WARN_MOCK_* variable  in this class, and turn off this one by default).

--- src/java/edu/mit/csail/pag/amock/jmock/MockObjectTestCase.java
+++ src/java/edu/mit/csail/pag/amock/jmock/MockObjectTestCase.java
@@ -2,14 +2,18 @@ package edu.mit.csail.pag.amock.jmock;
 
 import org.jmock.Sequence;
 import org.jmock.lib.objenesis.ObjenesisImposteriser;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 
 public abstract class MockObjectTestCase
     extends org.jmock.integration.junit3.MockObjectTestCase {
     {
         setImposteriser(ObjenesisImposteriser.INSTANCE);
     }
+
+    // Allow different levels of warnings for things that are tough to
+    // mock.  (Probably should use some logging framework.)
+    private static final boolean WARN_MOCKED_PUBLIC_FIELDS = true;
+    private static final boolean WARN_MOCKED_FINAL_METHODS = false;
 
     protected Sequence s = sequence("ordering");
 
@@ -24,13 +28,36 @@ public abstract class MockObjectTestCase
     }
 
     private <T> void checkMockability(Class<T> typeToMock) {
-        for (Field f : typeToMock.getFields()) {
-            int mods = f.getModifiers();
-            if (Modifier.isPublic(mods) && !Modifier.isStatic(mods)) {
-                // XXX maybe only care about non-final fields?
-                System.err.println("WARNING: Trying to mock class " +
-                                   typeToMock + " with public field: "
-                                   + f);
+        // Check for public fields, which can be read without us
+        // being able to mock them.
+        if (WARN_MOCKED_PUBLIC_FIELDS) {
+            for (Field f : typeToMock.getFields()) {
+                int mods = f.getModifiers();
+                if (Modifier.isPublic(mods) && !Modifier.isStatic(mods)) {
+                    // XXX maybe only care about non-final fields?
+                    System.err.println("WARNING: Trying to mock class " +
+                                       typeToMock + " with public field: "
+                                       + f);
+                }
+            }
+        }
+
+        // Check for final methods, which won't be mocked (and in
+        // fact, attempts to set expectations on them will actually
+        // call the original method).
+        if (WARN_MOCKED_FINAL_METHODS) {
+            for (Method m : typeToMock.getMethods()) {
+                if (m.getDeclaringClass() == Object.class) {
+                    // OK, so there are a bunch of final methods in
+                    // java.lang.Object.  We don't care.
+                    continue;
+                }
+                int mods = m.getModifiers();
+                if (Modifier.isFinal(mods) && !Modifier.isStatic(mods)) {
+                    System.err.println("WARNING: Trying to mock class " +
+                                       typeToMock + " with final method: "
+                                       + m);
+                }
             }
         }
     }

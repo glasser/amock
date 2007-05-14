358f9bc818c859c8f35a513fb8fe4e9770993876
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-05-14 05:11:51 +0000

    Reinstate (in slightly different form) representation for doesNotNeedDeclaration

--- trunk/src/java/edu/mit/csail/pag/amock/representation/Mocked.java
+++ trunk/src/java/edu/mit/csail/pag/amock/representation/Mocked.java
@@ -4,6 +4,8 @@ public class Mocked implements ProgramObject {
     private final String classSourceName;
     private final String varBaseName;
 
+    private boolean needsDeclaration = true;
+
     public Mocked(String classSourceName, String varBaseName) {
         this.classSourceName = classSourceName;
         this.varBaseName = varBaseName;
@@ -14,17 +16,33 @@ public class Mocked implements ProgramObject {
     }
 
     public String getMockVariableName() {
-        return "mock" + varBaseName;
+        if (needsDeclaration()) {
+            return "mock" + varBaseName;
+        } else {
+            return mockCall();
+        }
     }
 
     // In jMock 1, we needed separate variables for mocks and proxies;
     // not so in jMock 2.
     public String getProxyVariableName() {
-        return "mock" + varBaseName;
+        return getMockVariableName();
     }
 
     // Implements ProgramObject method.
     public String getSourceRepresentation() {
         return getProxyVariableName();
+    }
+
+    public boolean needsDeclaration() {
+        return needsDeclaration;
+    }
+
+    public void doesNotNeedDeclaration() {
+        needsDeclaration = false;
+    }
+
+    public String mockCall() {
+        return "mock(" + getClassSourceName() + ".class)";
     }
 }

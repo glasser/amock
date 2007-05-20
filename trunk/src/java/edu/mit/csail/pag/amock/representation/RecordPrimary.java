5336b04c60dfbfe3482567e7e15b86cd0adc8cdb
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-05-20 04:43:02 +0000

    Make RecordPrimary pull from field reads to constructor arguments... but hardcoded for java.awt.Rectangle.

--- trunk/src/java/edu/mit/csail/pag/amock/representation/RecordPrimary.java
+++ trunk/src/java/edu/mit/csail/pag/amock/representation/RecordPrimary.java
@@ -2,6 +2,7 @@ package edu.mit.csail.pag.amock.representation;
 
 import java.util.*;
 import edu.mit.csail.pag.amock.trace.TraceField;
+import edu.mit.csail.pag.amock.trace.Primitive;
 import edu.mit.csail.pag.amock.util.MultiSet;
 
 /**
@@ -11,6 +12,9 @@ import edu.mit.csail.pag.amock.util.MultiSet;
  */
 
 public class RecordPrimary extends AbstractPrimary {
+    private final Map<TraceField, ProgramObject> fieldValues
+        = new HashMap<TraceField, ProgramObject>();
+    
     public RecordPrimary(String classSourceName,
                          String varBaseName) {
         super(classSourceName, varBaseName);
@@ -21,11 +25,34 @@ public class RecordPrimary extends AbstractPrimary {
     // to tweaking state?  Dunno.
     public void haveFieldValue(TraceField field,
                                ProgramObject value) {
-        System.err.println("hFV: " + this + "; " + field + "; " + value);
+        if (fieldValues.containsKey(field)) {
+            // XXX this is wrong: should be OK if it's the same!
+            System.err.println("duplicate hFV: " + this + "; " + field + "; " + value);
+        } else {
+            fieldValues.put(field, value);
+        }
     }
 
+    private static final TraceField F_X
+        = new TraceField("java/awt/Rectangle", "x", "I");
+    private static final TraceField F_Y
+        = new TraceField("java/awt/Rectangle", "y", "I");
+    private static final TraceField F_WIDTH
+        = new TraceField("java/awt/Rectangle", "width", "I");
+    private static final TraceField F_HEIGHT
+        = new TraceField("java/awt/Rectangle", "height", "I");
+
     protected List<ProgramObject> getConstructorArguments() {
-        // NEXT:
-        return Collections.emptyList();
+        List<ProgramObject> args = new ArrayList<ProgramObject>();
+
+        for (TraceField f : new TraceField[] { F_X, F_Y, F_WIDTH, F_HEIGHT }) {
+            if (fieldValues.containsKey(f)) {
+                args.add(fieldValues.get(f));
+            } else {
+                args.add(new Primitive(0));
+            }
+        }
+
+        return args;
     }
 }

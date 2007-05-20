fb0a07058bae2aa1ca7f5433c2071fadfec8b94a
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-05-20 23:57:36 +0000

    Work in progress (test fails because it passes in a null) for doing method-based RecordPrimary stuff.  Still VERY HARD-CODED.

--- trunk/src/java/edu/mit/csail/pag/amock/representation/RecordPrimary.java
+++ trunk/src/java/edu/mit/csail/pag/amock/representation/RecordPrimary.java
@@ -1,8 +1,7 @@
 package edu.mit.csail.pag.amock.representation;
 
 import java.util.*;
-import edu.mit.csail.pag.amock.trace.TraceField;
-import edu.mit.csail.pag.amock.trace.Primitive;
+import edu.mit.csail.pag.amock.trace.*;
 import edu.mit.csail.pag.amock.util.MultiSet;
 
 /**
@@ -11,13 +10,46 @@ import edu.mit.csail.pag.amock.util.MultiSet;
  * which are generally not stateful.
  */
 
+// BRAINSTORM
+//
+// the right idea is basically a map from field or method to
+// constructor slot.
+// also need to default for the others.
+
 public class RecordPrimary extends AbstractPrimary {
-    private final Map<TraceField, ProgramObject> fieldValues
-        = new HashMap<TraceField, ProgramObject>();
-    
+    private final List<ProgramObject> fieldValues
+        = new ArrayList<ProgramObject>();
+
+    // vs rectangle
+    private final boolean isMouseEvent;
+
     public RecordPrimary(String classSourceName,
                          String varBaseName) {
         super(classSourceName, varBaseName);
+        // note: source name isn't actually enough information!!! loses package
+        if (classSourceName.equals("MouseEvent")) {
+            isMouseEvent = true;
+        } else {
+            assert classSourceName.equals("Rectangle");
+            isMouseEvent = false;
+        }
+
+        if (isMouseEvent) {
+            fieldValues.add(new Primitive(null));
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(false));
+        } else {
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(0));
+            fieldValues.add(new Primitive(0));
+        }
+
     }
 
     // This method makes the primary have the given field value.  If
@@ -25,34 +57,57 @@ public class RecordPrimary extends AbstractPrimary {
     // to tweaking state?  Dunno.
     public void haveFieldValue(TraceField field,
                                ProgramObject value) {
-        if (fieldValues.containsKey(field)) {
-            // XXX this is wrong: should be OK if it's the same!
-            System.err.println("duplicate hFV: " + this + "; " + field + "; " + value);
-        } else {
-            fieldValues.put(field, value);
+        assert !isMouseEvent;
+        if (!RECTANGLE_FIELDS.containsKey(field)) {
+            System.err.println("unknown field!: " + field);
+            return;
         }
+        
+        int index = RECTANGLE_FIELDS.get(field);
+//         if (fieldValues.get(field)) {
+//             // XXX this is wrong: should be OK if it's the same!
+//             System.err.println("duplicate hFV: " + this + "; " + field + "; " + value);
+//         } else {
+        fieldValues.set(index, value);
     }
 
-    private static final TraceField F_X
-        = new TraceField("java/awt/Rectangle", "x", "I");
-    private static final TraceField F_Y
-        = new TraceField("java/awt/Rectangle", "y", "I");
-    private static final TraceField F_WIDTH
-        = new TraceField("java/awt/Rectangle", "width", "I");
-    private static final TraceField F_HEIGHT
-        = new TraceField("java/awt/Rectangle", "height", "I");
+    // This method makes the primary as if it had returned from the given
+    // method.  If it's inconsistent with what it's been told before,
+    // maybe revert to tweaking state?  Dunno.
+    public void returnsFromMethod(TraceMethod method,
+                                  ProgramObject value) {
+        assert isMouseEvent;
+        if (!MOUSEEVENT_METHODS.containsKey(method)) {
+            System.err.println("unknown method!: " + method);
+            return;
+        }
+        
+        int index = MOUSEEVENT_METHODS.get(method);
+//         if (fieldValues.get(field)) {
+//             // XXX this is wrong: should be OK if it's the same!
+//             System.err.println("duplicate hFV: " + this + "; " + field + "; " + value);
+//         } else {
+        fieldValues.set(index, value);
+    }
+
+    private static final Map<TraceField, Integer> RECTANGLE_FIELDS
+        = new HashMap<TraceField, Integer>();
+    private static final Map<TraceMethod, Integer> MOUSEEVENT_METHODS
+        = new HashMap<TraceMethod, Integer>();
+
+    static {
+        RECTANGLE_FIELDS.put(new TraceField("java/awt/Rectangle", "x", "I"), 0);
+        RECTANGLE_FIELDS.put(new TraceField("java/awt/Rectangle", "y", "I"), 1);
+        RECTANGLE_FIELDS.put(new TraceField("java/awt/Rectangle", "width", "I"), 2);
+        RECTANGLE_FIELDS.put(new TraceField("java/awt/Rectangle", "height", "I"), 3);
+
+        MOUSEEVENT_METHODS.put(new TraceMethod("java/awt/event/MouseEvent",
+                                               "getX", "()I"), 4);
+        MOUSEEVENT_METHODS.put(new TraceMethod("java/awt/event/MouseEvent",
+                                               "getY", "()I"), 5);
+    }
 
     protected List<ProgramObject> getConstructorArguments() {
-        List<ProgramObject> args = new ArrayList<ProgramObject>();
-
-        for (TraceField f : new TraceField[] { F_X, F_Y, F_WIDTH, F_HEIGHT }) {
-            if (fieldValues.containsKey(f)) {
-                args.add(fieldValues.get(f));
-            } else {
-                args.add(new Primitive(0));
-            }
-        }
-
-        return args;
+        return Collections.unmodifiableList(fieldValues);
     }
 }

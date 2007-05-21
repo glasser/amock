8a47255ff112654b505d1351c4e88db41454b84a
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-05-21 14:14:25 +0000

    Extract ProgramObjectFactory interface from TestMethodGenerator.

---
+++ trunk/src/java/edu/mit/csail/pag/amock/representation/ProgramObjectFactory.java
@@ -0,0 +1,19 @@
+package edu.mit.csail.pag.amock.representation;
+
+import edu.mit.csail.pag.amock.trace.*;
+
+public interface ProgramObjectFactory {
+    public Mocked addMock(String className);
+    public Primary addPrimary(String className,
+                              ProgramObject[] pos,
+                              boolean explicit);
+    public RecordPrimary addRecordPrimary(String className,
+                                          boolean explicit);
+    public PrimaryExecution addPrimaryExecution(Primary p,
+                                                TraceMethod m,
+                                                ProgramObject... arguments);
+    public Expectation addExpectation(Mocked m, Integer count);
+    public void tweakState(Mocked receiver,
+                           TraceField field,
+                           ProgramObject value);
+}

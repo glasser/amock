e6e736ef5d48f2f70b24f701805cf89f70343cd8
David Glasser <glasser@davidglasser.net>
2007-05-29 16:48:53 +0000

    Make the "Book System" have two variants: one of which needs to be treated as Mocked, and the other of which can be RecordPrimary.  Should probably add a ProcessorTests case to ensure that RecordPrimary is actually used!

--- subjects/src/edu/mit/csail/pag/amock/subjects/fields/FieldSystem.java
+++ subjects/src/edu/mit/csail/pag/amock/subjects/fields/FieldSystem.java
@@ -3,16 +3,31 @@ package edu.mit.csail.pag.amock.subjects.fields;
 import edu.mit.csail.pag.amock.tests.AmockUnitTestCase;
 
 public class FieldSystem {
-    public static void main(String[] args) {
+    public static class MakeMock {
+        public static void main(String[] args) {
+            doIt(true);
+        }
+        public static class ProcessorTests extends AmockUnitTestCase {
+            // TODO: add processor tests
+            public void testNothing() { }
+        }
+    }
+    
+    public static class MakeRP {
+        public static void main(String[] args) {
+            doIt(false);
+        }
+        public static class ProcessorTests extends AmockUnitTestCase {
+            // TODO: add processor tests
+            public void testNothing() { }
+        }
+    }
+    
+    public static void doIt(boolean readIt) {
         Book b = new Book("Infinite Jest");
         Library l = new Library(b);
         Patron p = new Patron();
 
-        p.browseAndCheckOut(l);
-    }
-
-    public static class ProcessorTests extends AmockUnitTestCase {
-        // TODO: add processor tests
-        public void testNothing() { }
+        p.browseAndCheckOut(l, readIt);
     }
 }
--- subjects/src/edu/mit/csail/pag/amock/subjects/fields/Patron.java
+++ subjects/src/edu/mit/csail/pag/amock/subjects/fields/Patron.java
@@ -1,9 +1,16 @@
 package edu.mit.csail.pag.amock.subjects.fields;
 
 public class Patron {
-    public void browseAndCheckOut(Library l) {
+    public void browseAndCheckOut(Library l, boolean readIt) {
         Book b = l.browse();
         l.checkOut(b.title);
-        b.readOutLoud();
+
+        // The idea is: readOutLoud is not a benign method, and needs
+        // to be mocked, so iterations with readIt true should have
+        // Book be a mock, whereas iterations with readIt false should
+        // have Book be a RecordPrimary.
+        if (readIt) {
+            b.readOutLoud();
+        }
     }
 }

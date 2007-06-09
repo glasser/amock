92483964f4d74090828e675431f25d07fd29e37d
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-06-09 22:47:46 +0000

    Add a way for the processor (or TMG or whatever) to tell a ProgramObject what types it needs to be able to act like.

--- trunk/src/java/edu/mit/csail/pag/amock/representation/ProgramObject.java
+++ trunk/src/java/edu/mit/csail/pag/amock/representation/ProgramObject.java
@@ -1,5 +1,7 @@
 package edu.mit.csail.pag.amock.representation;
 
+import org.objectweb.asm.Type;
+
 /**
  * Represents something that can be manipulated in the generated test;
  * for example, a primary object, mock, or primitive.  Anything that
@@ -7,4 +9,12 @@ package edu.mit.csail.pag.amock.representation;
  */
 public interface ProgramObject {
     public String getSourceRepresentation();
+
+    /**
+     * Inform the ProgramObject that it must be capable of being used
+     * in a context where an object of the given type is expected (in
+     * the generated test).  (Can be used to calculate the most
+     * general possible type for a mock, etc.)
+     */
+    public void usedAsType(Type t);
 }

0ddb08272e8ac1d99234743270aa816a56d6e91b
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-06-18 01:43:28 +0000

    r151515@30-5-39:  glasser | 2007-06-17 17:54:23 -0400  * Factor some stuff from TraceTransformer into a CustomGeneratorAdapter.
     * Pass class name through to Smock.

--- trunk/src/java/edu/mit/csail/pag/smock/Smock.java
+++ trunk/src/java/edu/mit/csail/pag/smock/Smock.java
@@ -1,10 +1,15 @@
 package edu.mit.csail.pag.smock;
 
+import org.jmock.api.Invocation;
+
 public class Smock {
     // needs class, args too
 
-    public static Result maybeMockStaticMethod(String name, String desc) {
-        System.err.format("HELP I AM IN A STATIC METHOD %s!!!\n", name);
+    public static Result maybeMockStaticMethod(String className,
+                                               String name,
+                                               String desc) {
+        System.err.format("HELP I AM IN A STATIC METHOD %s.%s!!!\n",
+                          className, name);
         if (name.equals("getSomeNumber")) {
             return new Result(true, 38);
         }

7b517746bb1d20bb6edc5952e24d9726aacdc204
David Glasser <glasser@davidglasser.net>
2007-06-19 16:45:22 +0000

    Use custom Expectations subclass.

---
+++ src/java/edu/mit/csail/pag/smock/Expectations.java
@@ -0,0 +1,8 @@
+package edu.mit.csail.pag.smock;
+
+public class Expectations extends org.jmock.Expectations {
+//     public <T> T one(Class<T> invokedClass) {
+//         Object invocationReceiver = super.one((Object) invokedClass);
+//         return null;
+//     }
+}

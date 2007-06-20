e6f36c5b9c44b59120d4f8c0a4161ffa662cdbcb
dglasser <dglasser@9e9fc4b9-8027-0410-b9b7-d3651de2fb66>
2007-06-20 01:27:19 +0000

    give teeth to the static method amockify test

--- trunk/subjects/src/edu/mit/csail/pag/amock/subjects/staticmethod/OtherClass.java
+++ trunk/subjects/src/edu/mit/csail/pag/amock/subjects/staticmethod/OtherClass.java
@@ -2,15 +2,15 @@ package edu.mit.csail.pag.amock.subjects.staticmethod;
 
 public class OtherClass {
     public static int getSomeNumber() {
-//         try {
-//             throw new RuntimeException();
-//         } catch (RuntimeException e) {
-//             for (StackTraceElement ste : e.getStackTrace()) {
-//                 if (ste.getClassName().equals("junit.framework.TestSuite")) {
-//                     throw new RuntimeException("should have been mocked out!");
-//                 }
-//             }
-//         }
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException e) {
+            for (StackTraceElement ste : e.getStackTrace()) {
+                if (ste.getClassName().equals("junit.framework.TestSuite")) {
+                    throw new RuntimeException("should have been mocked out!");
+                }
+            }
+        }
 
         return 42;
 

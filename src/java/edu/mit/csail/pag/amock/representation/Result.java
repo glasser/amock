ceda448f4dadfc9c0e1b1f8124d1c33ec9bcc1ba
David Glasser <glasser@davidglasser.net>
2007-06-24 00:36:10 +0000

    work-in-progress ResultsClause refactor

---
+++ src/java/edu/mit/csail/pag/amock/representation/Result.java
@@ -0,0 +1,3 @@
+package edu.mit.csail.pag.amock.representation;
+
+public interface Result extends CodeChunk {}
---
+++ src/java/edu/mit/csail/pag/amock/representation/ReturnValueResult.java
@@ -0,0 +1,28 @@
+package edu.mit.csail.pag.amock.representation;
+
+import edu.mit.csail.pag.amock.util.MultiSet;
+
+public class ReturnValueResult implements Result {
+    private final ProgramObject returnValue;
+
+    public ReturnValueResult(ProgramObject returnValue) {
+        this.returnValue = returnValue;
+    }
+
+    public void resolveNames(ClassNameResolver cr,
+                             VariableNameBaseResolver vr) {
+        returnValue.resolveNames(cr, vr);
+    }
+
+    public void printSource(LinePrinter p) {
+        p.line("returnValue(" + returnValue.getExpectationReturnValueRepresentation()
+               + ")");
+    }
+
+    public MultiSet<ProgramObject> getProgramObjects() {
+        MultiSet<ProgramObject> pos = new MultiSet<ProgramObject>();
+
+        pos.add(returnValue);
+        return pos;
+    }
+}

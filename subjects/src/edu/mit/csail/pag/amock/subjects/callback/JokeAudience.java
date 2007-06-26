502e0fe710c7af6fd33e3dabd53780b7eb6fe574
David Glasser <glasser@davidglasser.net>
2007-06-26 03:40:29 +0000

    Ensure that JokeAudience test really requires callbacks to happen in order to pass.

--- subjects/src/edu/mit/csail/pag/amock/subjects/callback/JokeAudience.java
+++ subjects/src/edu/mit/csail/pag/amock/subjects/callback/JokeAudience.java
@@ -2,13 +2,16 @@ package edu.mit.csail.pag.amock.subjects.callback;
 
 public class JokeAudience {
     private final JokeTeller jt;
-        
+    private boolean laughed = false;
+
     public JokeAudience(JokeTeller jt) {
         this.jt = jt;
     }
 
     public String listenToJoke() {
-        return jt.knockKnock(this);
+        String response = jt.knockKnock(this);
+        assert laughed;
+        return response;
     }
 
     public String whosThere() {
@@ -16,6 +19,7 @@ public class JokeAudience {
     }
 
     public String who(String knocker) {
+        laughed = true;
         return "they said: " + jt.punchline(knocker);
     }
 }

a4bef4f52e81a3808edbb3cdb015a2e8c4b9e96c
David Glasser <glasser@davidglasser.net>
2007-07-25 18:48:57 +0000

    Start adding Derby

---
+++ subjects/in/derby/sample.sql
@@ -0,0 +1,6 @@
+connect 'jdbc:derby:subjects/out/derby/AmockDB;create=true';
+create table derbyDB(num int, addr varchar(40));
+insert into derbyDB values (1956, 'Webster St.');
+insert into derbyDB values (1910, 'Union St.');
+update derbyDB set num=180, addr='Grand Ave.' where num=1956;
+select * from derbyDb;

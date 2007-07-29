b70184d810a43f6cbbe97b98e7ccfc8e6769d840
David Glasser <glasser@davidglasser.net>
2007-07-29 20:44:15 +0000

    tasks for jmodeller crunch

---
+++ build/jmodeller_tasks.rb
@@ -0,0 +1,282 @@
+unit_test(:jm1, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = ''
+  u.tested_class = 'AssociationLineConnection'
+end
+
+unit_test(:jm2, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = ''
+  u.tested_class = 'ClassFigure'
+end
+
+unit_test(:jm3, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = ''
+  u.tested_class = 'DelegationSelectionTool'
+end
+
+unit_test(:jm4, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = ''
+  u.tested_class = 'DependencyLineConnection'
+end
+
+unit_test(:jm5, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = ''
+  u.tested_class = 'InheritanceLineConnection'
+end
+
+unit_test(:jm6, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = ''
+  u.tested_class = 'JModellerApplication'
+end
+
+unit_test(:jm7, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = ''
+  u.tested_class = 'JModellerClass'
+end
+
+unit_test(:jm8, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = ''
+  u.tested_class = 'SeparatorFigure'
+end
+
+unit_test(:jm9, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.contrib'
+  u.tested_class = 'GraphicalCompositeFigure'
+end
+
+unit_test(:jm10, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.contrib'
+  u.tested_class = 'MDI_InternalFrame'
+end
+
+unit_test(:jm11, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.contrib'
+  u.tested_class = 'StandardLayouter'
+end
+
+unit_test(:jm12, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.figures'
+  u.tested_class = 'ArrowTip'
+end
+
+unit_test(:jm13, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.figures'
+  u.tested_class = 'ConnectedTextTool'
+end
+
+unit_test(:jm14, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.figures'
+  u.tested_class = 'FigureAttributes'
+end
+
+unit_test(:jm15, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.figures'
+  u.tested_class = 'GroupCommand'
+end
+
+unit_test(:jm16, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.figures'
+  u.tested_class = 'RectangleFigure'
+end
+
+unit_test(:jm17, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.figures'
+  u.tested_class = 'TextFigure'
+end
+
+unit_test(:jm18, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.figures'
+  u.tested_class = 'TextTool'
+end
+
+unit_test(:jm19, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.figures'
+  u.tested_class = 'UngroupCommand'
+end
+
+unit_test(:jm20, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.framework'
+  u.tested_class = 'DrawingChangeEvent'
+end
+
+unit_test(:jm21, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.framework'
+  u.tested_class = 'FigureChangeEvent'
+end
+
+unit_test(:jm22, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'BringToFrontCommand'
+end
+
+unit_test(:jm23, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'ChangeAttributeCommand'
+end
+
+unit_test(:jm24, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'ChopBoxConnector'
+end
+
+unit_test(:jm25, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'ConnectionTool'
+end
+
+unit_test(:jm26, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'CopyCommand'
+end
+
+unit_test(:jm27, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'CutCommand'
+end
+
+unit_test(:jm28, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'DeleteCommand'
+end
+
+unit_test(:jm29, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'DuplicateCommand'
+end
+
+unit_test(:jm30, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'FigureChangeEventMulticaster'
+end
+
+unit_test(:jm31, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'FigureEnumerator'
+end
+
+unit_test(:jm32, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'PasteCommand'
+end
+
+unit_test(:jm33, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'ReverseFigureEnumerator'
+end
+
+unit_test(:jm34, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'SendToBackCommand'
+end
+
+unit_test(:jm35, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'SimpleUpdateStrategy'
+end
+
+unit_test(:jm36, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'StandardDrawing'
+end
+
+unit_test(:jm37, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'StandardDrawingView'
+end
+
+unit_test(:jm38, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'ToggleGridCommand'
+end
+
+unit_test(:jm39, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.standard'
+  u.tested_class = 'ToolButton'
+end
+
+unit_test(:jm40, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.util'
+  u.tested_class = 'ColorEntry'
+end
+
+unit_test(:jm41, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.util'
+  u.tested_class = 'CommandMenu'
+end
+
+unit_test(:jm42, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.util'
+  u.tested_class = 'Iconkit'
+end
+
+unit_test(:jm43, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.util'
+  u.tested_class = 'PaletteIcon'
+end
+
+unit_test(:jm44, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.util'
+  u.tested_class = 'ReverseVectorEnumerator'
+end
+
+unit_test(:jm45, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.util'
+  u.tested_class = 'SerializationStorageFormat'
+end
+
+unit_test(:jm46, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.util'
+  u.tested_class = 'StandardStorageFormat'
+end
+
+unit_test(:jm47, JMODELLER_TRACE, JMODELLER_II, JMODELLER_HIERARCHY,
+          [JMODELLER_II, JMODELLER_TRACE]) do |u|
+  u.package = 'CH.ifa.draw.util'
+  u.tested_class = 'StorageFormatManager'
+end
+
